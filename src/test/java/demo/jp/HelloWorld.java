c6eccfb2f9305b6872dedb54d9d621c7e7076350
JAYAPRAKASAN <prakasah.abi@gmail.com>
2025-05-13 05:05:08 +0530

    Revert "committing May 13"
    This reverts commit ef113c2521af3d80a93c65dc13c0be01fb8e01ee.

--- src/test/java/demo/jp/HelloWorld.java
+++ src/test/java/demo/jp/HelloWorld.java
@@ -8,7 +8,5 @@ class HelloWorld
 	public static void main(String args[])
 	{
 		System.out.println("Hello, World");
-		System.out.println("Hello1, World");
-		System.out.println("Hello2, World");
 	}
 }
