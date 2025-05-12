ef113c2521af3d80a93c65dc13c0be01fb8e01ee
JAYAPRAKASAN <prakasah.abi@gmail.com>
2025-05-13 05:04:03 +0530

    committing May 13

--- src/test/java/demo/jp/HelloWorld.java
+++ src/test/java/demo/jp/HelloWorld.java
@@ -8,5 +8,7 @@ class HelloWorld
 	public static void main(String args[])
 	{
 		System.out.println("Hello, World");
+		System.out.println("Hello1, World");
+		System.out.println("Hello2, World");
 	}
 }
